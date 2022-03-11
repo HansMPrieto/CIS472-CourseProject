e5c209553285041b3f979b5860cf00d1356f61af
Evan Podrabsky <epodrabs@uoregon.edu>
2022-03-11 13:41:27 -0800

    Updated ibrahim notebook

--- models/vgg.py
+++ models/vgg.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 
 class VGG16(nn.Module):
-    def __init__(self, hidden_features, nout):
+    def __init__(self, hidden_features, nout, dropout=False):
         super().__init__()
+        self.dropout = dropout
         self.conv_1a = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3,3), padding=1)
         nn.init.kaiming_uniform_(self.conv_1a.weight)
         self.conv_1b = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3,3), padding=1)
@@ -25,7 +26,7 @@ class VGG16(nn.Module):
         self.linear_1 = nn.Linear(512*2*2, hidden_features)
         self.linear_2 = nn.Linear(hidden_features, hidden_features)
         self.linear_3 = nn.Linear(hidden_features, nout)
-        self.dropout = nn.Dropout()
+        self.drop = nn.Dropout()
         self.batchnorm_64 = nn.BatchNorm2d(64)
         self.batchnorm_128 = nn.BatchNorm2d(128)
         self.batchnorm_256 = nn.BatchNorm2d(256)
@@ -62,8 +63,10 @@ class VGG16(nn.Module):
 #         x = self.batchnorm_512(x)
         x = x.view(x.shape[0], -1)
         x = self.relu(self.linear_1(x))
-#         x = self.dropout(x)
+        if self.dropout:
+            x = self.drop(x)
         x = self.relu(self.linear_2(x))
-#         x = self.dropout(x)
+        if self.dropout:
+            x = self.drop(x)
         x = self.linear_3(x)
         return x
