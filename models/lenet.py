ee36dc7439b4c87c9d3b3f3c29d6a9265af60598
HansMPrieto <hansmiguelpprieto@gmail.com>
2022-03-11 00:13:53 -0800

    Added LeNet Training for Ibrahim Dataset

--- models/lenet.py
+++ models/lenet.py
@@ -10,7 +10,7 @@ class LeNet(nn.Module):
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, num_classes)
-
+        
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.max_pool2d(x, 2)
